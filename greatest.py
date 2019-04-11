3a719f73142ef30f1f113bb6596afca35161417d
Nandheeswarraja <49307544+Nandheeswarraja@users.noreply.github.com>
2019-04-11 16:40:15 +0530

    Create greatest.py

---
+++ greatest.py
@@ -0,0 +1,9 @@
+numb1=int(input("Enter a number1"))
+numb2=int(input("Enter a number2"))
+numb3=int(input("Enter a number3"))
+if(numb1>numb2)&(numb1>numb3):
+print("the number 1 is greatest")
+elif(numb2>numb3)&(numb2>numb1):
+print("the number 2 is greatest")
+else:
+print("the number 3 is greatest")
