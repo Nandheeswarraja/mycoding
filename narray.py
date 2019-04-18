3a920969773256b50e4bbb4b01538f8c033b8937
Nandheeswarraja <49307544+Nandheeswarraja@users.noreply.github.com>
2019-04-18 11:09:22 +0530

    Create narray.py

---
+++ narray.py
@@ -0,0 +1,18 @@
+def check_power(n,k):
+if n<=0 or k<=0:
+   print "not valid input"
+else:
+   for i in range(1,18):
+     x=k**i
+     if x==n:
+       print"true"
+       print k,"power","is",n
+       break
+    elif
+       print "false"
+       break
+check_Power(244140625,5)
+check_Power(4096,16)
+check_Power(0,16)
+check_Power(1,1)
+       
