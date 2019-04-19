a1fa77d54faf611f960479bf16d9191a18c9acf6
Nandheeswarraja <49307544+Nandheeswarraja@users.noreply.github.com>
2019-04-19 12:09:14 +0530

    Update armstronginterval.py

--- armstronginterval.py
+++ armstronginterval.py
@@ -4,8 +4,8 @@ for num in range(lower,upper + 1):
    sum = 0
    temp = num
    while temp > 0:
-       digit = temp % 10
-       sum += digit ** 3
-       temp //= 10
-   if num == sum:
+      digit = temp % 10
+      sum += digit ** 3
+      temp //= 10
+     if num == sum:
        print(num)
