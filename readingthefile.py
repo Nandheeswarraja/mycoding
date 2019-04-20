959b48b22579e8f019f5a5311144edd27246a998
Nandheeswarraja <49307544+Nandheeswarraja@users.noreply.github.com>
2019-04-20 15:29:02 +0530

    Update readingthefile.py

--- readingthefile.py
+++ readingthefile.py
@@ -2,3 +2,11 @@ FileName = ("Path\para.txt")
 with open(FileName, 'r') as file:
     lines_in_file = file.read()
     print lines_in_file 
+import nltk
+FileName = ("Path\para.txt")
+with open(FileName, 'r') as file:
+    lines_in_file = file.read()   
+    nltk_tokens = nltk.word_tokenize(lines_in_file)
+    print nltk_tokens
+    print "\n"
+    print "Number of Words: " , len(nltk_tokens)
