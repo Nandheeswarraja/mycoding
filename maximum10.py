2f3bf435e96120bf41333c19eff631d5d72bf2c6
Nandheeswarraja <49307544+Nandheeswarraja@users.noreply.github.com>
2019-04-21 08:58:55 +0530

    Update maximum10.py

--- maximum10.py
+++ maximum10.py
@@ -1,6 +1,6 @@
 lst = []
-num = int(input('How many numbers: '))
-for n in range(num):
-    numbers = int(input('Enter number '))
-    lst.append(numbers)
+number = int(input('How many numbers: '))
+for n in range(number):
+    num = int(input('Enter number '))
+    lst.append(num)
 print("Maximum element in the list is :", max(lst), "\nMinimum element in the list is :", min(lst))
